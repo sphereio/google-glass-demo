38c5c3059c030886d21a9745fa775d930886af8d
Carlos Muñoz <youchy@gmail.com>
2014-11-05 13:30:56 +0100

    ADD Alternatives model

---
+++ app/src/main/java/com/sphere/io/glass/model/Alternative.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package com.sphere.io.glass.model;
+
+/**
+ * Created with Android Studio .
+ * User: Carlos Muñoz Romero
+ * Date: 5/11/14
+ * Time: 12:28
+ * Project: SphereGlass
+ */
+public class Alternative {
+
+    private int id;
+
+    private String name;
+
+    private String imageUrl;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
