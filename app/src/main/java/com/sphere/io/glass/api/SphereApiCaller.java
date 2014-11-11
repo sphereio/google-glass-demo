0db0c5d0b0ecce07d71b2965c2b02c11ba03315a
fvillalbacar <f.villalba@ingravitymedia.com>
2014-11-11 17:14:46 +0100

    modified query to fit the one working on tests

--- app/src/main/java/com/sphere/io/glass/api/SphereApiCaller.java
+++ app/src/main/java/com/sphere/io/glass/api/SphereApiCaller.java
@@ -21,14 +21,14 @@ import retrofit.converter.GsonConverter;
  */
 public class SphereApiCaller {
 
-    private RestAdapter mRestAdapter;
+    private  static RestAdapter mRestAdapter;
     private static  SphereApiCaller mSphereApiCaller;
     //KNOWLEDGE PURPOSES
     private static final String CLIENT_ID = "654GYqPJccG9X3KCrKImwLyO";
     private static final String CLIENT_SECRET = "bAS3tW-PfDpNbxlqSIiUJ7H-XDOo6BIN";
     private static final String KEY_AUTHORIZATION = "Authorization";
     private static final String AUTHORIZATION = "Basic NjU0R1lxUEpjY0c5WDNLQ3JLSW13THlPOmJBUzN0Vy1QZkRwTmJ4bHFTSWlVSjdILVhET282QklO";
-    private static final String AUTHORIZATION_BEARER = "Bearer";
+    private static final String AUTHORIZATION_BEARER = "Bearer ";
     private static final String GRANT_TYPE = "client_credentials";
     private static final String SCOPE = "manage_project:google-glass-demo";
     private static final String PERMISSIONS = "manage_project:google-glass-demo";
@@ -51,10 +51,27 @@ public class SphereApiCaller {
                         if (authSession == null) {
                             request.addHeader(KEY_AUTHORIZATION, AUTHORIZATION);
                         } else {
+                            generateRestAdapter(context);
+                        }
+                    }
+                })
+                .setConverter(new GsonConverter(gson))
+                .build();
+    }
+
+    private static void generateRestAdapter(final Context context){
+        Gson gson = new GsonBuilder()
+                .create();
+        mRestAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL)
+        .setEndpoint(context.getString(R.string.sphere_api_endpoint))
+
+                .setRequestInterceptor(new RequestInterceptor() {
+                    @Override
+                    public void intercept(RequestFacade request) {
+                        Session authSession = SpherePreferenceManager.getInstance(context).getSession();
                             request.addHeader(KEY_AUTHORIZATION, new StringBuffer(AUTHORIZATION_BEARER)
                                     .append(authSession.getToken()).toString());
 
-                        }
                     }
                 })
                 .setConverter(new GsonConverter(gson))
@@ -64,6 +81,10 @@ public class SphereApiCaller {
     public static SphereApiCaller getInstance(Context context) {
         if (mSphereApiCaller == null) {
             mSphereApiCaller = new SphereApiCaller(context);
+        }
+        Session authSession = SpherePreferenceManager.getInstance(context).getSession();
+        if (authSession != null) {
+            generateRestAdapter(context);
         }
         return mSphereApiCaller;
     }
