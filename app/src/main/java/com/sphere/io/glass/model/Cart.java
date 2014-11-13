6e9ffa79c63371fb978b88dc106cb230a0170cea
fvillalbacar <f.villalba@ingravitymedia.com>
2014-11-13 17:04:29 +0100

    ADD cart model

---
+++ app/src/main/java/com/sphere/io/glass/model/Cart.java
@@ -0,0 +1,18 @@
+
+package com.sphere.io.glass.model;
+
+/**
+ * Created by Francisco Villalba on 13/11/14.
+ */
+public class Cart {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
