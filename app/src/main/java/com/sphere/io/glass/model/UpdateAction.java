9fc5e061feceac0308e96a9b600aa16e5855aef8
fvillalbacar <f.villalba@ingravitymedia.com>
2014-11-17 13:04:45 +0100

    ADD more models

---
+++ app/src/main/java/com/sphere/io/glass/model/UpdateAction.java
@@ -0,0 +1,29 @@
+package com.sphere.io.glass.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Francisco Villalba on 14/11/14.
+ */
+public class UpdateAction {
+        private int version;
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public ArrayList<Action> getActions() {
+        return actions;
+    }
+
+    public void setActions(ArrayList<Action> actions) {
+        this.actions = actions;
+    }
+
+    private ArrayList<Action> actions;
+
+}
